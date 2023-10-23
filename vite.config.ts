89aba449915d38e31888cbe059be465a54ea8626
ruizavi <avi.ruiz@proton.me>
2023-10-23 14:29:48 -0600

    change name in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { resolve } from "node:path";
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react-swc";
-import dts from "vite-plugin-dts";
+import { resolve } from 'node:path';
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
-  build: {
-    lib: {
-      entry: resolve("src", "index.tsx"),
-      name: "cold-plasma",
-      formats: ["es", "umd"],
-    },
-  },
-  test: {
-    environment: "happy-dom",
-  },
+   plugins: [react(), dts({ rollupTypes: true })],
+   build: {
+      lib: {
+         entry: resolve('src', 'index.tsx'),
+         name: 'birdies',
+         formats: ['es', 'umd']
+      }
+   },
+   test: {
+      environment: 'happy-dom'
+   }
 });
