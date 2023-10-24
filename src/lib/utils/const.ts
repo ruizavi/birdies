4a71d3aa26da6532bcb0c0cf1de3d3631692aadd
ruizavi <avi.ruiz@proton.me>
2023-10-23 21:03:34 -0600

    feature: add basic const, colors and size options, and class prefix

---
+++ src/lib/utils/const.ts
@@ -0,0 +1,4 @@
+export const CLASS_PREFIX = 'bird';
+
+export type COLORS = 'pink' | 'orange' | 'deep' | 'red' | 'blue' | 'yellow' | 'green' | 'purple';
+export type SIZES = 'xs' | 'sm' | 'md' | 'lg';
