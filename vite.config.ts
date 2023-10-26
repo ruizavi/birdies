2e2e6bc0dd40aa1a423891cd900f3e5640217e19
ruizavi <avi.ruiz@proton.me>
2023-10-26 12:51:27 -0600

    fix: imports in projects

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,18 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
+import { peerDependencies } from './package.json';
 
 export default defineConfig({
    plugins: [react(), dts({ rollupTypes: true })],
    build: {
       lib: {
-         entry: resolve('src', 'lib', 'index.tsx'),
+         entry: resolve('src', 'index.tsx'),
          name: 'birdies',
          formats: ['es', 'umd']
+      },
+      rollupOptions: {
+         external: [...Object.keys(peerDependencies)]
       }
    },
    test: {
