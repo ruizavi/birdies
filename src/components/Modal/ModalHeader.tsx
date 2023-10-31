5611dd2c5dc63c973ac64cb15e125cb00db11208
ruizavi <avi.ruiz@proton.me>
2023-10-31 13:36:24 -0600

    feature: modal header component function

--- src/components/Modal/ModalHeader.tsx
+++ src/components/Modal/ModalHeader.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AsProps } from '../../utils/types';
+import useClass from '../../utils/useClass';
 
 export interface ModalHeaderProps extends AsProps<'header'>, React.HTMLAttributes<HTMLElement> {
    classPrefix?: string;
@@ -7,10 +8,27 @@ export interface ModalHeaderProps extends AsProps<'header'>, React.HTMLAttribute
 }
 
 const ModalHeader = React.forwardRef<HTMLHeadElement, ModalHeaderProps>((props, ref) => {
+   const {
+      role = 'heading',
+      classPrefix = 'modal-header',
+      as = 'header',
+      className,
+      children,
+      handleClose,
+      ...rest
+   } = props;
+
+   const { addClassPrefix, merge } = useClass(classPrefix);
+
+   const classes = merge(className, addClassPrefix());
+
+   const Component = as || 'header';
+
    return (
-      <header ref={ref}>
-         header <button onClick={props.handleClose}>x</button>{' '}
-      </header>
+      <Component {...rest} role={role} className={classes} ref={ref}>
+         {children}
+         <button onClick={handleClose}>x</button>
+      </Component>
    );
 });
 
