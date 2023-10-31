7e476b113f1dd54477eb5aee26d808e144e2797a
ruizavi <avi.ruiz@proton.me>
2023-10-31 13:42:25 -0600

    feature: modal body component function

--- src/components/Modal/ModalBody.tsx
+++ src/components/Modal/ModalBody.tsx
@@ -1,5 +1,31 @@
+import React from 'react';
 import { AsProps } from '../../utils/types';
+import useClass from '../../utils/useClass';
 
 export interface ModalBodyProps extends AsProps<'main'>, React.HTMLAttributes<HTMLElement> {
    classPrefix: string;
 }
+
+const ModalBody = React.forwardRef<HTMLHeadingElement, ModalBodyProps>((props, ref) => {
+   const { role, classPrefix = 'modal-body', as, className, children, ...rest } = props;
+
+   const { addClassPrefix, merge } = useClass(classPrefix);
+
+   const classes = merge(className, addClassPrefix());
+
+   const Component = as || 'main';
+
+   const render = () => {
+      return <>{children}</>;
+   };
+
+   return (
+      <Component {...rest} role={role} className={classes} ref={ref}>
+         {render()}
+      </Component>
+   );
+});
+
+ModalBody.displayName = 'Modal-Title';
+
+export default ModalBody;
