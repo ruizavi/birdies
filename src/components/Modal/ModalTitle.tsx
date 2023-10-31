ace64b5e5600559dba3dd7fadcf175eef5ad1aa2
ruizavi <avi.ruiz@proton.me>
2023-10-31 13:41:01 -0600

    feature: modal title component function

--- src/components/Modal/ModalTitle.tsx
+++ src/components/Modal/ModalTitle.tsx
@@ -1,5 +1,31 @@
+import React from 'react';
 import { AsProps } from '../../utils/types';
+import useClass from '../../utils/useClass';
 
 export interface ModalTitleProps extends AsProps<'h4'>, React.HTMLAttributes<HTMLElement> {
    classPrefix: string;
 }
+
+const ModalTitle = React.forwardRef<HTMLHeadingElement, ModalTitleProps>((props, ref) => {
+   const { role, classPrefix = 'modal-title', as, className, children, ...rest } = props;
+
+   const { addClassPrefix, merge } = useClass(classPrefix);
+
+   const classes = merge(className, addClassPrefix());
+
+   const Component = as || 'h4';
+
+   const render = () => {
+      return <>{children}</>;
+   };
+
+   return (
+      <Component {...rest} role={role} className={classes} ref={ref}>
+         {render()}
+      </Component>
+   );
+});
+
+ModalTitle.displayName = 'Modal-Title';
+
+export default ModalTitle;
