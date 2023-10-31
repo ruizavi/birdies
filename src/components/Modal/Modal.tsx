67a99704aa40477c5e085bd7049350aed7af52d1
ruizavi <avi.ruiz@proton.me>
2023-10-31 13:49:06 -0600

    feature: extend subcomponents of modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ModalHeader from './ModalHeader';
 import ModalBase, { ModalProps } from './ModalBase';
+import ModalTitle from './ModalTitle';
+import ModalBody from './ModalBody';
+import ModalFooter from './ModalFooter';
 
 interface ModalComponent
    extends React.ForwardRefExoticComponent<ModalProps & React.RefAttributes<HTMLDivElement>> {
    Header: typeof ModalHeader;
+   Title: typeof ModalTitle;
+   Body: typeof ModalBody;
+   Footer: typeof ModalFooter;
 }
 
 const Modal = {
    ...ModalBase,
-   Header: ModalHeader
+   Header: ModalHeader,
+   Title: ModalTitle,
+   Body: ModalBody,
+   Footer: ModalFooter
 } as ModalComponent;
 
 export default Modal;
