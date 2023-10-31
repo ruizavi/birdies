1776213c37cf0972d62086ed95f9f90c9b89021c
ruizavi <avi.ruiz@proton.me>
2023-10-31 13:43:43 -0600

    feature: modal footer component function

--- src/components/Modal/ModalFooter.tsx
+++ src/components/Modal/ModalFooter.tsx
@@ -1,5 +1,31 @@
+import React from 'react';
 import { AsProps } from '../../utils/types';
+import useClass from '../../utils/useClass';
 
 export interface ModalFooterProps extends AsProps<'footer'>, React.HTMLAttributes<HTMLElement> {
    classPrefix: string;
 }
+
+const ModalFooter = React.forwardRef<HTMLHeadingElement, ModalFooterProps>((props, ref) => {
+   const { role, classPrefix = 'modal-footer', as, className, children, ...rest } = props;
+
+   const { addClassPrefix, merge } = useClass(classPrefix);
+
+   const classes = merge(className, addClassPrefix());
+
+   const Component = as || 'footer';
+
+   const render = () => {
+      return <>{children}</>;
+   };
+
+   return (
+      <Component {...rest} role={role} className={classes} ref={ref}>
+         {render()}
+      </Component>
+   );
+});
+
+ModalFooter.displayName = 'Modal-Title';
+
+export default ModalFooter;
